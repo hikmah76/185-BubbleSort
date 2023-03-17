2cc530459165decaad6f66caea96e1a0e2c1d292
hikmah76 <nur.hikmah190504@gmail.com>
2023-03-17 20:56:21 +0700

    memasukkan variabel menambahkan "void input" menambahkan “void display” menambahkan “void bubbleSortArray” menampilkan output

--- BubbleSort/bubblesort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/bubblesort/BubbleSort/BubbleSort.cpp
@@ -1,62 +1,62 @@
 #include <iostream>
-using namepace std;
+using namespace std;
 
 
-int a[20]             // Deklarasi array a deangan ukuran 20
-int n;               // Deklarasi variabel n untuk menyimpan banyak elemen pada array
+int a[20];				// Deklarasi array a dengan ukuran 20
+int n;					// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 
-void input() {         // procedure untuk input
-	while (true) {     // looping 
-		cout << "Masukan banyaknya elemen pada array : "; // Output ke layar
-		cin >> n;     // Input dari pengguna 
-		if (n <= 20) // Jika n kurang dari atau sama dengan 20
-			break   // Keluar dari loop
-		else (     // Jika n lebih dari 20
-			count << "\nArray dapat mempunyai maksimal 20 elemen.\n"; //Output ke layar
-	   }
+void input() {			// Procedure untuk input
+	while (true) {		// Looping
+		cout << "Masukkan banyaknya elemen pada array : "; // Output ke layar
+		cin >> n;		// Input dari pengguna
+		if (n <= 20)	// Jika n kurang dari atau sama dengan 20
+			break;		// Keluar dari loop
+		else {			// Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai  maksimal 20 elemen.\n"; // Output ke layar
+		}
 	}
-    cout << end1;                                 // Output baris kosong
-	cout << "===================" << end1;       // Output ke layar
-	cout << "Masukan Elemen Array" << end1;     // Output ke layar
-	cout << "===================" << end1;     // Output ke layar
+	cout << endl;							// Output baris kosong
+	cout << "===================" << endl;	// Output ke layar
+	cout << "Masukan Elemen Array" << endl; // Output ke layar
+	cout << "===================" << endl;	// Output ke layar
 
-	for (int i = 0; 1 < n; 1++) {   // Looping dengan i dimulai dari 0 hingga n-1
-		cout << "Data ke-" << (i + 1) << ": ";  // Output ke layar 
-		cin >> a[i]                     // Input dari pengguna 
+	for (int i = 0; i < n; i++) {	// Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data Ke-" << (i + 1) << ": ";	// Output ke layar
+		cin >> a[i];
 	}
 }
 
-void dispaly() {  //procedure untuk menampilkan hasil 
-	cout << end1;                                 // Output baris kosong
-	cout << "==================================" << end1;       // Output ke layar
-	cout << "Element Array yang telah tersusun" << end1;     // Output ke layar
-	cout << "==================================" << end1;     // Output ke layar
-	for (int j = 0; j < n; j++) {   // Looping dengan j dimulai dari 0 hingga n-1
-		cout << a[j] << end1;      // Output ke layar 
+void display() {	// Procedure untuk menampilkan hasil
+	cout << endl;									// Output baris kosong
+	cout << "==================================" << endl;	 // Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;	// Output ke layar
+	cout << "==================================" << endl;	// Output ke layar
+	for (int j = 0; j < n; j++) {					// Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;						// Output ke layar
 	}
-	cout << end1                  // Output baris kosong
+	cout << endl;									// Output baris kosong
 }
 
-void bubbleSortArray() {   // Procedure untuk menggurutkan array deangan metode bubble sort 
-	for (int i = 1; i < n; i++) {      // Looping dengan i dimulai dari 1 hingga n-1
-		for (int j = 0; j < n - i; j++) {  // Looping dengan j dimulai dari 0 hingga n-i-1
-			if (a[j] > a[j + 1]) {      // Jika nilai pada a[j] lebih besar dari a[j+1]
-				int temp = a[j];       // Simpan nilai a[j] ke variabel  sementara temp
-				a[j] = a[j + 1];      // Assign nilai a[j+1] ke a[j]
-				a[j + 1] = temp;     // Assign nilai temp ke a[j+1]
+void bubbleSortArray() {	// Procedure untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {			// Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) {	// Looping dengan j dimulai dari 0 hingga n-i
+			if (a[j] > a[j + 1]) {			// Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];			// Simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];			// Assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;			// Assign nilai temp ke a[j+1]
 			}
 		}
 	}
 }
 
 
-int main () {
-	input();     // Memanggil read() dari class BubbleSort
-	bubbleSortArray();  // Memanggil bubbleSortArray() dari class BubbleSort
-	display();   // Memanggil display() dari class BubbleSort
+int main() {
+	input();	// Memanggil read()
+	bubbleSortArray();	// Memanggil bubbleSortArray()
+	display();	// Memanggil display()
 	return 0;
-
+}
 
 
 	
