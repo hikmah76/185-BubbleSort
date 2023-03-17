a9a8eff56b873762e0d7d2706f176e63aa261d59
hikmah76 <nur.hikmah190504@gmail.com>
2023-03-17 18:28:19 +0700

    void display

--- BubbleSort/bubblesort/BubbleSort/BubbleSort.cpp
+++ BubbleSort/bubblesort/BubbleSort/BubbleSort.cpp
@@ -16,6 +16,31 @@ void input() {         // procedure untuk input
 			count << "\nArray dapat mempunyai maksimal 20 elemen.\n"; //Output ke layar
 	   }
 	}
-cout << end1;
-    cout << "==================="
+    cout << end1;                                 // Output baris kosong
+	cout << "===================" << end1;       // Output ke layar
+	cout << "Masukan Elemen Array" << end1;     // Output ke layar
+	cout << "===================" << end1;     // Output ke layar
+
+	for (int i = 0; 1 < n; 1++) {   // Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";  // Output ke layar 
+		cin >> a[i]                     // Input dari pengguna 
+	}
+}
+
+void dispaly() {  //procedure untuk menampilkan hasil 
+	cout << end1;                                 // Output baris kosong
+	cout << "==================================" << end1;       // Output ke layar
+	cout << "Element Array yang telah tersusun" << end1;     // Output ke layar
+	cout << "==================================" << end1;     // Output ke layar
+	for (int j = 0; j < n; j++) {   // Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << end1;      // Output ke layar 
+	}
+	cout << end1                  // Output baris kosong
+}
+
+void bubbleSortArray() {   // Procedure untuk menggurutkan array deangan metode bubble sort 
+	for (int i = 1; i < n; i++) {      // Looping dengan i dimulai dari 1 hingga n-i-1
+		if ()
+
+
 
